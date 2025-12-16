577790908f186cd345235b6c3b6d5b37c57e1639
KawaiianPizza <Kawaiinpizza@gmail.com>
2025-12-16 15:18:08 -0800

    Update twitch.tv Clip redirect.user.js

--- twitch.tv Clip redirect.user.js
+++ twitch.tv Clip redirect.user.js
@@ -2,11 +2,11 @@
 // @name        twitch.tv Clip redirect
 // @namespace   Violentmonkey Scripts
 // @match       https://www.twitch.tv/*/clip/*
-// @version     1.0
+// @version     1.0.1
 // @author      Kawaiian Pizza
 // @run-at      document-start
 // @downloadURL https://github.com/KawaiianPizza/Scripts/raw/main/twitch.tv%20Clip%20redirect.user.js
 // @homepageURL https://github.com/KawaiianPizza/Scripts/raw/main/twitch.tv%20Clip%20redirect.user.js
 // @description 12/16/2025, 12:43:59 PM
 // ==/UserScript==
-location = `https://clips.twitch.tv${location.pathname.slice(location.pathname.lastIndexOf('/'))}`
+location.replace(`https://clips.twitch.tv${location.pathname.slice(location.pathname.lastIndexOf('/'))}`)
